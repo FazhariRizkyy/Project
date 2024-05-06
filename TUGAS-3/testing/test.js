52b4eba3b10393a41eaff67accc09981c62264b4
Fazhari <160480037+FazhariRizkyy@users.noreply.github.com>
2024-05-06 10:51:51 +0700

    Add files via upload

---
+++ TUGAS-3/testing/test.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  function generateCaptcha() {
+    const firstNumber = Math.floor(Math.random() * 10);
+    const secondNumber = Math.floor(Math.random() * 10);
+    $("#firstNumber").text(firstNumber);
+    $("#secondNumber").text(secondNumber);
+  }
+
+  $("#loginButton").click(function () {
+    window.location.href = "/CV.html";
+  });
+
+  $("#loginButton").click(function () {
+    var dbemail = "user@mail.com";
+    var dbpass = "user";
+    var email = $("#inputEmail").val();
+    var pass = $("#inputPassword").val();
+    var captchaResult = parseInt($("#captchaResult").val());
+    var firstNumber = parseInt($("#firstNumber").text());
+    var secondNumber = parseInt($("#secondNumber").text());
+
+    if (dbemail === email && dbpass === pass) {
+      if (captchaResult === firstNumber + secondNumber) {
+        alert("Login Success");
+      } else {
+        alert("UPS Captcha salah");
+      }
+    } else {
+      alert("Invalid Email or Password");
+    }
+  });
+
+  $("#captchaResult").focus();
+  generateCaptcha();
+});
